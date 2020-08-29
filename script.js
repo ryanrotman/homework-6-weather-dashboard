2071a24035420f03223e4d820e9d0d53e7589780
Ryan Rotman <ryan.rotman@gmail.com>
2020-08-29 01:01:46 -0500

    added code for building searched cities list and storing those cities in local storage

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Global variables for needed elements on the page
 var currentDay = moment().format("l");
 console.log("Current Day: " + currentDay);
+var searchedCitiesArray = [];
 
 // Click event for #search-btn
 $("#search-btn").on("click", function() {
@@ -13,6 +14,24 @@ $("#search-btn").on("click", function() {
     console.log("User Search for: " + city);
     // Clear the input field
     $("#search-input").val("");
+    // Add search to #city-list
+    var newCitySearch = $("<div>");
+    newCitySearch.addClass("row list-new-city");
+    newCitySearch.attr("id", city + "-search");
+    newCitySearch.html('<a href="#">' + city + '</a>');
+    $("#city-list").append(newCitySearch);
+    // Saving search list to local storage
+    var searchedCities = {
+        "city": city,
+    };
+    console.log(searchedCities);
+    var searchedCityList = JSON.parse(localStorage.getItem("searchedCities"));
+    if (searchedCityList == null ) {
+        searchedCityList = [];
+    }
+    searchedCityList.push(searchedCities);
+    console.log(searchedCityList);
+    localStorage.setItem("searchedCities", JSON.stringify(searchedCityList));
     // Construct a url to search opencage to get the latitude and longitude coordinates for the city
     var openCageQueryURL = "https://api.opencagedata.com/geocode/v1/json?q=" + city + "&key=" + apiKeyOpenCage;
     // Perform first of two AJAX calls using $.when() to get user inputed city then latitude and longitude settings for weather information
@@ -64,10 +83,63 @@ $("#search-btn").on("click", function() {
             } else if (currentUVI >= 11) {
                 $("#uv-index").css("background-color", "violet");
             };
+            // Add date to forecast cards
+            var day1Date = moment().add(1, "days").format("l");
+            var day2Date = moment().add(2, "days").format("l");
+            var day3Date = moment().add(3, "days").format("l");
+            var day4Date = moment().add(4, "days").format("l");
+            var day5Date = moment().add(5, "days").format("l");
+            $("#day1-date").html(day1Date);
+            $("#day2-date").html(day2Date);
+            $("#day3-date").html(day3Date);
+            $("#day4-date").html(day4Date);
+            $("#day5-date").html(day5Date);
+            // Add weather icon to forecast cards
+            $("#day1-weather-icon").attr("src", "https://openweathermap.org/img/wn/" + response.daily[0].weather[0].icon + ".png");
+            $("#day1-weather-icon").attr("alt", response.daily[0].weather[0].description);
+            $("#day2-weather-icon").attr("src", "https://openweathermap.org/img/wn/" + response.daily[1].weather[0].icon + ".png");
+            $("#day2-weather-icon").attr("alt", response.daily[1].weather[0].description);
+            $("#day3-weather-icon").attr("src", "https://openweathermap.org/img/wn/" + response.daily[2].weather[0].icon + ".png");
+            $("#day3-weather-icon").attr("alt", response.daily[2].weather[0].description);
+            $("#day4-weather-icon").attr("src", "https://openweathermap.org/img/wn/" + response.daily[3].weather[0].icon + ".png");
+            $("#day4-weather-icon").attr("alt", response.daily[3].weather[0].description);
+            $("#day5-weather-icon").attr("src", "https://openweathermap.org/img/wn/" + response.daily[4].weather[0].icon + ".png");
+            $("#day5-weather-icon").attr("alt", response.daily[4].weather[0].description);
+            // Add temperature to forecast cards
+            $("#day1-temperature").html(response.daily[0].temp.max + " &#x2109;");
+            $("#day2-temperature").html(response.daily[1].temp.max + " &#x2109;");
+            $("#day3-temperature").html(response.daily[2].temp.max + " &#x2109;");
+            $("#day4-temperature").html(response.daily[3].temp.max + " &#x2109;");
+            $("#day5-temperature").html(response.daily[4].temp.max + " &#x2109;");
+            // Add humidity to forecast cards
+            $("#day1-humidity").html(response.daily[0].humidity + "&#37;");
+            $("#day2-humidity").html(response.daily[1].humidity + "&#37;");
+            $("#day3-humidity").html(response.daily[2].humidity + "&#37;");
+            $("#day4-humidity").html(response.daily[3].humidity + "&#37;");
+            $("#day5-humidity").html(response.daily[4].humidity + "&#37;");
         });
     }));
 });
 
+function showSearchedCities() {
+    searchedCitiesArray = JSON.parse(localStorage.getItem("searchedCities"));
+    console.log("Searched Cities Array: ", searchedCitiesArray);
+    if (searchedCitiesArray != null) {
+        for (var i = 0; i < searchedCitiesArray.length; i++) {
+            var searchedCity = searchedCitiesArray[i];
+            console.log("Selected City from Array: ", searchedCity);
+            var citySearched = searchedCity.city;
+            $(citySearched + "-search").html('<a href="#">' + citySearched + '</a>');
+        };
+    };
+};
+
+// Call showSearchedCities function
+showSearchedCities();
+
 /* TODO:
-    - for future development make app more accurate by being able to grab weather info for exact city the user wants
+    - for future development
+        - make app more accurate by being able to grab weather info for exact city the user wants
+        - adjust code that populate the 5-day forecast cards
+            - use loop to populate data?
 */
